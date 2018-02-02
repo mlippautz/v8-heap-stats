d09c321deb89a9a99f983e8cd28e81fd883f95a9
Michael Lippautz <mlippautz@chromium.org>
2018-02-02 09:52:51 +0100

    Remove old tool and update README to point to V8's repository

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # V8 Heap Statistics
+
+This repository hosts the heap stats visualizer developed at https://chromium.googlesource.com/v8/v8.
--- app/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";  // eslint-disable-line no-unused-vars
-import ReactDOM from "react-dom";
-
-import V8HeapStats from "./v8-heap-stats";  // eslint-disable-line no-unused-vars
-
-ReactDOM.render(
-  <V8HeapStats name="World"/>,
-  document.getElementById('content')
-);
